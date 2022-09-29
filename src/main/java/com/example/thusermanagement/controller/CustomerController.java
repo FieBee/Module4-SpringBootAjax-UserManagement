0ef67a4bdec6c67121ef6e11847cca605cc89adf
FieBee <phuonglf98@gmail.com>
2022-09-29 14:55:45 +0700

    alo

--- src/main/java/com/example/thusermanagement/controller/CustomerController.java
+++ src/main/java/com/example/thusermanagement/controller/CustomerController.java
@@ -40,31 +40,21 @@ public class CustomerController {
 
 
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Customer> findById(@PathVariable Long id){
-//        Optional<Customer> customer =customerService.findById(id);
-//        if (!customer.isPresent()){
-//            return new  ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<>(customer.get(),HttpStatus.OK);
-//    }
-
-    @GetMapping("/create")
-    public ModelAndView showCreate(){
-        return new ModelAndView("/customer/create");
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Long id){
+        Optional<Customer> customer =customerService.findById(id);
+        if (!customer.isPresent()){
+            return new  ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(customer.get(),HttpStatus.OK);
     }
+
     @PostMapping
     public ResponseEntity<Customer> save(@RequestBody Customer customer){
         return new ResponseEntity<>(customerService.save(customer), HttpStatus.CREATED);
     }
 
 
-//    @GetMapping("/edit/{id}")
-//    public ModelAndView showUpdate(@PathVariable Long id){
-//        ModelAndView modelAndView = new ModelAndView("/customer/edit");
-//        modelAndView.addObject("customer", customerService.findById(id));
-//        return modelAndView;
-//    }
     @PutMapping("/{id}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,@RequestBody Customer customer){
         Optional<Customer> customerOptional = customerService.findById(id);
@@ -76,12 +66,7 @@ public class CustomerController {
     }
 
 
-//    @GetMapping("/delete/{id}")
-//    public ModelAndView showDelete(@PathVariable Long id){
-//        ModelAndView modelAndView = new ModelAndView("/customer/delete");
-//        modelAndView.addObject("customer", customerService.findById(id));
-//        return modelAndView;
-//    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id){
         Optional<Customer> customerOptional = customerService.findById(id);
@@ -93,72 +78,4 @@ public class CustomerController {
     }
 
 
-//    @PutMapping("{id}")
-//    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,@RequestBody Customer customer){
-//
-//    }
-
-
-//    @GetMapping("/create-customer")
-//    public ModelAndView showCreateForm() {
-//        ModelAndView modelAndView = new ModelAndView("/customer/create");
-//        modelAndView.addObject("customer", new Customer());
-//        return modelAndView;
-//    }
-//
-//    @PostMapping("/create-customer")
-//    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
-//        customerService.save(customer);
-//        ModelAndView modelAndView = new ModelAndView("/customer/create");
-//        modelAndView.addObject("customer", new Customer());
-//        modelAndView.addObject("message", "New customer created successfully");
-//        return modelAndView;
-//    }
-//
-//    @GetMapping("/customers")
-//    public ModelAndView listCustomers(){
-//        ModelAndView modelAndView = new ModelAndView("/customer/list");
-//        modelAndView.addObject("customers",customerService.findAll());
-//        return modelAndView;
-//    }
-//
-//    @GetMapping("/edit-customer/{id}")
-//    public ModelAndView showEditForm(@PathVariable Long id){
-//        Optional<Customer> customer = customerService.findById(id);
-//        if (customer.isPresent()){
-//            ModelAndView modelAndView = new ModelAndView("/customer/edit");
-//            modelAndView.addObject("customer",customer.get());
-//            return modelAndView;
-//        }else {
-//            return new ModelAndView("error.404");
-//        }
-//    }
-//
-//    @PostMapping("/edit-customer")
-//    public ModelAndView updateCustomer(@ModelAttribute("customer")Customer customer ){
-//        customerService.save(customer);
-//        ModelAndView modelAndView = new ModelAndView("/customer/edit");
-//        modelAndView.addObject("customer",customer);
-//        modelAndView.addObject("message","Customer updated successfully");
-//        return modelAndView;
-//    }
-//
-//    @GetMapping("/delete-customer/{id}")
-//    public ModelAndView showDeleteForm(@PathVariable Long id) {
-//        Optional<Customer> customer = customerService.findById(id);
-//        if (customer.isPresent()) {
-//            ModelAndView modelAndView = new ModelAndView("/customer/delete");
-//            modelAndView.addObject("customer", customer.get());
-//            return modelAndView;
-//
-//        } else {
-//            return new ModelAndView("/error.404");
-//        }
-//    }
-//
-//    @PostMapping("/delete-customer")
-//    public String deleteCustomer(@ModelAttribute("customer") Customer customer) {
-//        customerService.remove(customer.getId());
-//        return "redirect:customers";
-//    }
 }
